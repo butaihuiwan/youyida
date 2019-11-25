09d129a3838e40ced4f686422a76b61f95e7baf1
butaihuiwan <925747399@qq.com>
2019-11-25 16:16:32 +0800

    解决jenkins中运行 页面无法全屏 jenkins 部署在Tocat01

--- user_case/testcase_1report.py
+++ user_case/testcase_1report.py
@@ -17,19 +17,12 @@ class TestcaseReport(unittest.TestCase):
 
     def setUp(self) -> None:
         opt = webdriver.ChromeOptions()
-        # opt.headless = True
-        prefs = {"profile.managed_default_content_settings.images": 2}
-        opt.add_experimental_option("prefs", prefs)
-        # opt = Options()
-        # opt.add_argument('--no-sandbox')  # 解决DevToolsActivePort文件不存在的报错
-        # opt.add_argument('window-size=1920x3000')  # 指定浏览器分辨率
+        opt.headless = True
         opt.add_argument('--disable-gpu')  # 谷歌文档提到需要加上这个属性来规避bug
-        # opt.add_argument('--hide-scrollbars')  # 隐藏滚动条, 应对一些特殊页面
         opt.add_argument('blink-settings=imagesEnabled=false')  # 不加载图片, 提升速度
-        # # opt.add_argument('--headless')
         self.driver = webdriver.Chrome(options=opt)
         self.driver.get('http://192.168.17.50:2090/home/control/main')
-        self.driver.manage().window().setSize(1920, 1280)
+
         driver = self.driver
         driver.find_element_by_name('USERNAME').send_keys('customer1')
         driver.find_element_by_name('PASSWORD').send_keys('123456')
