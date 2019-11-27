f4d8af6a31fa56bef0486eeeead8f4b1f872616c
butaihuiwan <925747399@qq.com>
2019-11-27 17:08:04 +0800

    解决jenkins中运行 页面无法全屏 jenkins 部署在Tocat01

--- other/1.py
+++ other/1.py
@@ -9,7 +9,7 @@ def baidu():
     opt.headless = True
     opt.add_argument('--disable-gpu')  # 谷歌文档提到需要加上这个属性来规避bug
     opt.add_argument('blink-settings=imagesEnabled=false')  # 不加载图片, 提升速度
-    driver = webdriver.Chrome(options=opt)
+    driver = webdriver.Chrome(executable_path='/opt/google/chrome/chromedriver', options=opt)
     driver.get('http://192.168.17.50:2090/home/control/main')
     driver.find_element_by_name('USERNAME').send_keys('customer1')
     driver.find_element_by_name('PASSWORD').send_keys('123456')
@@ -18,5 +18,8 @@ def baidu():
     login.click()
     time.sleep(3)
     driver.maximize_window()
+    driver.get_screenshot_as_file(
+        "../s.png")
+
 
 baidu()
